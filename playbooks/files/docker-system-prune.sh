7353faef1f8dd029ad6902d1a06db9c0306d93fc
Daniel Schömer <daniel.schoemer@gmx.net>
2020-12-06 19:51:17 +0100

    Updated docker-system-prune.sh to remove unused volumes

--- playbooks/files/docker-system-prune.sh
+++ playbooks/files/docker-system-prune.sh
@@ -3,5 +3,7 @@
 systemctl status docker 1>/dev/null 2>&1 || STOP_DOCKER=1
 systemctl start docker
 docker system prune -f
+docker system prune --volume -f
+docker system prune --all -f
 
 [ "${STOP_DOCKER}" = "1" ] && systemctl stop docker
